9117c3e61ceb46f5db0ac0f87c6ace68d1229e69
Vladimir Jakubik <vladimir.jakubik77@gmail.com>
2024-01-26 10:21:41 +0100

    Add EventListener for pressing keyboard and create new function to play sound

--- index.js
+++ index.js
@@ -1,7 +1,48 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
 
+//EventListener for clicking on button
 for (var i=0; i< numberOfDrums; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        alert("Hello");
+        playSound(this.innerHTML);
     });
+}
+
+// EventListener for pressing key
+document.addEventListener("keydown", function (event) {
+    playSound(event.key);
+});
+
+// function to play sound based on pressed key or clicked button
+function playSound(whichLetter) {
+    switch (whichLetter) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;   
+        case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;  
+        case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;  
+        case "j":
+            var snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break; 
+        case "k":
+            var crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "l":
+            var kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break; 
+        default: console.log(buttonInnerHTML);    
+    }
 }
